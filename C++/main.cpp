ee4724cfce69ab2a68ed2cfa468b4655d9a33c34
Фарманов Магомед Альбертович <Farma_gi04@itstep.edu.az>
2023-05-02 17:53:17 +0400

    deleted bloat

--- C++/main.cpp
+++ C++/main.cpp
@@ -4,16 +4,29 @@ int main() {
 	using namespace std;
 	setlocale(LC_ALL, "Russian");
 
+	uint16_t choice = 0;
 	cout
 		<< "Выберите задание:" << endl
 		<< "1. Морской бой" << endl
 		<< "2. Пятнашки" << endl
 		<< "3. Cписок дел" << endl
-		<< "4. Игра Память" << endl
+		<< "4. Игра \'Память\'" << endl
 		<< "5. Подсчет арифметического выражения" << endl
-		<< "6. Приложение Каталог текстов песен" << endl
+		<< "6. Приложение \'Каталог текстов песен\'" << endl
 		<< "> ";
+	cin >> choice;
 
+	switch (choice)
+	{
+	case 1:
+
+
+		break;
+
+	default:
+		cout << "No such option" << endl;
+		break;
+	}
 	
 	return 0;
 }
