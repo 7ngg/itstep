0d13a6bd5086a0ced73f3ec7ab2a44b9155891f2
tng <m.farm4nov@gmail.com>
2023-05-22 23:47:08 +0300

    finalized prac for 05.21.2023

--- prac/prac_Farmanov_05.21.2023/hw_Farmanov_05.21.2023.cpp
+++ prac/prac_Farmanov_05.21.2023/hw_Farmanov_05.21.2023.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 class student
 {
@@ -19,9 +20,18 @@ public:
 	std::string schoolCity{}, schoolCountry{};
 	std::string group{};
 
+
 	birthDate getBirthDate() {
 		return studentBirthDate;
 	}
+
+
+	void setBirthDate(uint16_t day, uint16_t month, uint16_t year) {
+		this->studentBirthDate.day = day;
+		this->studentBirthDate.month = month;
+		this->studentBirthDate.year = year;
+	}
+
 
 	void print() {
 		std::cout
@@ -38,8 +48,44 @@ public:
 
 
 int main() {
-	
+	student newStudent{};
+	uint16_t dd{}, mm{}, yy{};
 
+	std::cout << "Enter student's name: ";
+	std::getline(std::cin, newStudent.name);
+	std::cout << "Enter student's surname: ";
+	std::getline(std::cin, newStudent.surname);
+
+	std::cout
+		<< "You are about to enter students birth date..." << '\n'
+		<< "Enter day of birth: ";
+	std::cin >> dd;
+	std::cout << "Enter month: ";
+	std::cin >> mm;
+	std::cout << "Enter year: ";
+	std::cin >> yy;
+	newStudent.setBirthDate(dd, mm, yy);
+
+	std::cin.ignore();
+
+	std::cout << "Enter contact number: ";
+	std::getline(std::cin, newStudent.phoneNumber);
+	std::cout << "Enter group: ";
+	std::getline(std::cin, newStudent.group);
+	std::cout << "City: ";
+	std::getline(std::cin, newStudent.city);
+	std::cout << "Country: ";
+	std::getline(std::cin, newStudent.country);
+
+	std::cout << "Enter school name: ";
+	std::getline(std::cin, newStudent.school);
+	std::cout << "City: ";
+	std::getline(std::cin, newStudent.schoolCity);
+	std::cout << "Country: ";
+	std::getline(std::cin, newStudent.schoolCountry);
+
+	system("cls");
+	newStudent.print();
 
 	return 0;
 }
