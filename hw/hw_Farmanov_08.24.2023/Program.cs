6de99357a7b488f0776a0f84ecc2e09443123239
tng <m.farm4nov@gmail.com>
2023-08-30 23:26:59 +0400

    first task of 08.24

---
+++ hw/hw_Farmanov_08.24.2023/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.Write("Select task: ");
+int taskChoice = Convert.ToInt32(Console.ReadLine());
+
+switch (taskChoice)
+{
+    case 1:
+    {
+        Console.Write("Enter length: ");
+        int length = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter character: ");
+        char filling = Convert.ToChar(Console.ReadLine());
+
+        for (int i = 0; i < length; i++)
+        {
+            Console.WriteLine(new string(filling, length));
+        }
+
+        break;
+    }
+
+    case 2:
+    {
+        break;
+    }
+
+    default:
+        break;
+}
