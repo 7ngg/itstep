ff3fee9aaa2b1f525bf0459961fcb82178b905db
tng <m.farm4nov@gmail.com>
2023-09-03 21:52:12 +0400

    some task from 08.31.2023

--- hw/hw_Farmanov_08.31.2023/Program.cs
+++ hw/hw_Farmanov_08.31.2023/Program.cs
@@ -1,4 +1,7 @@
-﻿using Integers;
+﻿using System.Data;
+using System.Security.Cryptography;
+using Integers;
+using Passport;
 
 Console.Write("Select task: ");
 int taskChoice = Convert.ToInt32(Console.ReadLine());
@@ -8,7 +11,19 @@ switch (taskChoice)
 {
     case 1:
     {
-
+        Console.Write("Enter number: ");
+        bool check = int.TryParse(Console.ReadLine(), out int num);
+        if (!check)
+        {
+            throw new ArgumentException();
+        }
+        Console.Write("Required number system: ");
+        check = int.TryParse(Console.ReadLine(), out int requiredNumberSystem);
+        if (!check)
+        {
+            throw new ArgumentException();
+        }
+        System.Console.WriteLine($"Result: {number_system_convert(num, requiredNumberSystem)}");
         break;
     }
 
@@ -17,16 +32,57 @@ switch (taskChoice)
         int num = Convert.ToInt32(Console.ReadLine());
         Numbers x = (Numbers)num;
         System.Console.WriteLine(x); 
-
         break;
     }
 
     case 3:
     {
-        
+        System.Console.Write("Firstname: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Lastname: ");
+        string lastName = Console.ReadLine();
+        PassportConfiguration passport = new(firstName, lastName);
+        passport.Print();
+        break;
+    }
+
+    case 4:     // https://sportishka.com/uploads/posts/2022-08/1661365210_1-sportishka-com-p-nakachennii-dogi-instagram-1.jpg
+    {
+        System.Console.Write("Expression: ");
+        string expression = Console.ReadLine();
+        string[] operatorsArray = new string[] { "<=", ">=", ">" , "<", "!=", "==" };
+        string[] numbers = expression.Split(operatorsArray, StringSplitOptions.None);
+
+        break;
+    }
+
+    case 5:     // https://kartinkof.club/uploads/posts/2022-03/1648387937_3-kartinkof-club-p-mem-slabaya-sobaka-3.png
+    {
+        string expression;
+
+        Console.Write("Expression: ");
+        expression = Console.ReadLine();
+
+        var result = new DataTable().Compute(expression, null);
+        System.Console.WriteLine($"Result: {result}");
         break;
     }
 
     default:
         break;
+}
+
+
+static int number_system_convert(int x, int requiredNumberSystem) {
+    int result = 0;
+    int order = 1;
+
+    while(x > 0)
+    {
+        result += (x % requiredNumberSystem) * order;
+        x /= requiredNumberSystem;
+        order *= 10;
+    }
+
+    return result;
 }
--- hw/hw_Farmanov_08.31.2023/passport.cs
+++ hw/hw_Farmanov_08.31.2023/passport.cs
@@ -17,18 +17,18 @@ namespace Passport
         {
             Random random= new Random();
             PassportNO = random.Next(99999999).ToString();
-            name = new LFP { firstName = _firstName, lastName = _lastName };
+            name = new LFP { firstName = _firstName ?? throw new ArgumentException(), lastName = _lastName ?? throw new ArgumentException() };
             issuedOn = DateOnly.FromDateTime(DateTime.Now);
             expiresOn = DateOnly.FromDateTime(DateTime.Now);
-            expiresOn.AddYears(4);
+            expiresOn = expiresOn.AddYears(4);
         }
 
 
         public void Print() {
             System.Console.WriteLine(
-                $"NO: {passportNO}" +
-                $"Name: {name.firstName}    LastName: {name.lastName}   Patronymic: {name.patronymic ?? "None"}" +
-                $""
+                $"NO: {PassportNO}\n" +
+                $"Name: {name.firstName}    Lastname: {name.lastName}   Patronymic: {name.patronymic ?? "None"}\n" +
+                $"Issued on: {issuedOn.ToString()}      Expires on: {expiresOn.ToString()}\n"
             );
         }
     }
