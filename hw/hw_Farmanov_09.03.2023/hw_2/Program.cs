314e7ccc9eb54c426fed4000c4e4d59a9826fb41
tng <m.farm4nov@gmail.com>
2023-09-10 12:38:19 +0400

    completed 09.03.2023

--- hw/hw_Farmanov_09.03.2023/hw_2/Program.cs
+++ hw/hw_Farmanov_09.03.2023/hw_2/Program.cs
@@ -1,4 +1,1 @@
-﻿using Trading;
-using Instruments;
-
-var instrument = new Violin();
+﻿
--- hw/hw_Farmanov_09.03.2023/hw_2/include/employees.cs
+++ hw/hw_Farmanov_09.03.2023/hw_2/include/employees.cs
@@ -14,42 +14,87 @@ namespace Employees
             Age = age;
         }
 
-
-        public abstract void Print();
+        public abstract void PrintInfo();
+        public abstract void PrintBio();
     }
 
     public class President : Worker
     {
         public President() : base(
-            "Name",
-            "Surname",
-            ""
+            "Presi",
+            "Dent",
+            "President bio",
+            49
         ) {}
 
-        public override void Print()
+        public override void PrintInfo()
         {
-            System.Console.WriteLine($"{FirstName} {LastName}");
+            System.Console.WriteLine($"{FirstName} {LastName}, {Age}");
+        }
+
+        public override void PrintBio()
+        {
+            System.Console.WriteLine($"President bio: {Bio}");
         }
     }
 
     public class Security : Worker
     {
-        public Security(string firstName, string lastName) : base(firstName, lastName)
+        public Security() : base(
+            "Secu",
+            "Rity",
+            "Security bio",
+            36
+        ) {}
+
+        public override void PrintBio()
         {
+            System.Console.WriteLine($"Security bio: {Bio}");
+        }
+
+        public override void PrintInfo()
+        {
+            System.Console.WriteLine($"{FirstName} {LastName}, {Age}");
         }
     }
 
     public class Manager : Worker
     {
-        public Manager(string firstName, string lastName) : base(firstName, lastName)
+        public Manager() : base(
+            "Mana",
+            "Ger",
+            "Manager bio",
+            24
+        ) {}
+
+        public override void PrintInfo()
         {
+            System.Console.WriteLine($"{FirstName} {LastName}, {Age}");
+        }
+
+        public override void PrintBio()
+        {
+            System.Console.WriteLine($"Manager bio: {Bio}");
         }
     }
 
     public class Engineer : Worker
     {
-        public Engineer(string firstName, string lastName) : base(firstName, lastName)
+        public Engineer() : base(
+            "Engi",
+            "Neer",
+            "Engineer bio",
+            34
+        ) {}
+
+        public override void PrintInfo()
         {
+            System.Console.WriteLine($"{FirstName} {LastName}, {Age}");
+        }
+
+        public override void PrintBio()
+        {
+            System.Console.WriteLine($"Engineer bio: {Bio}");
         }
     }
 }
