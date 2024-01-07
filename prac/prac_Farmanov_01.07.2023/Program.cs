659a0c4db516acb9e156943541667cc4dd4baaf6
farma_gi04 <farma_gi04@itstep.edu.az>
2024-01-07 15:47:38 +0400

    prac for 01.07.2024

---
+++ prac/prac_Farmanov_01.07.2023/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder();
+builder.AddJsonFile("app-settings.json");
+var config = builder.Build();
+var connectionString =  config.GetConnectionString("step");
+
+string scalarQuery = "select * from People";
+string noQueryQuery = "insert into People(Name, Surname, Age) values('Name1', 'Surname1', 37)";
+
+using SqlConnection connection = new(connectionString);
+using SqlCommand cmd = new(scalarQuery, connection);
+
+try
+{
+    connection.Open();
+    System.Console.WriteLine(cmd.ExecuteScalar());;
+}
+catch (System.Exception e)
+{    
+    System.Console.WriteLine(e.Message);
+}
+
+
+Console.ReadLine();
