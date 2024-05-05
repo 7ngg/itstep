7fc7e31d2461e2f96836b06abc35cc18836c98f4
tng <m.farm4nov@gmail.com>
2024-05-05 21:56:16 +0400

    task2: reduce method

--- hw/hw_Farmanov_05.05.2024/script.js
+++ hw/hw_Farmanov_05.05.2024/script.js
@@ -27,80 +27,143 @@ let Vehicle = {
 
 //#region Task2
 
+
 function Fraction(numerator, denominator) {
-  this.Numerator = numerator;
-  this.Denominator = denominator;
+    this.Numerator = numerator;
+    this.Denominator = denominator;
 }
 
 function getDenominator(frac_1, frac_2) {
-  return frac_1.Denominator === frac_2.Denominator
-    ? frac_1.Denominator
-    : frac_1.Denominator * frac_2.Denominator;
+    return frac_1.Denominator === frac_2.Denominator
+        ? frac_1.Denominator
+        : frac_1.Denominator * frac_2.Denominator;
 }
 
 function getDivisors(num) {
-  let divisors = [];
-  for (let i = 1; i < num - 1; i++) {
-    if (num % i === 0) {
-      divisors.push(i);
+    let divisors = [];
+    for (let i = 1; i < num - 1; i++) {
+        if (num % i === 0) {
+            divisors.push(i);
+        }
     }
-  }
 
-  return divisors;
+    return divisors;
 }
 
-function reduceFraction(frac) {
-  let numeratorDivisors = getDivisors(frac.Numerator);
-  let denominatorDivisors = getDivisors(frac.Denominator);
-
-  numeratorDivisors.forEach((n) => {
-    if (denominatorDivisors.includes(n)) {
-      denominatorDivisors.pop(n);
-      numeratorDivisors.pop(n);
+Fraction.prototype.reduce = function() {
+    if (this.Numerator === 0) {
+        this.Denominator = 0;
+        return;
     }
-  });
-}
 
-Fraction.prototype.add = function (other) {
-  let newDenominator = getDenominator(this, other);
+    let gcd = (a, b) => {
+        while (b !== 0) {
+            let temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    };
 
-  let newNumerator =
-    this.Denominator === other.Denominator
-      ? this.Numerator + other.Numerator
-      : (newDenominator / other.Denominator) * this.Numerator +
-        (newDenominator / this.Denominator) * other.Numerator;
-
-  return new Fraction(newNumerator, newDenominator);
+    let greatestCommonDivisor = gcd(this.Numerator, this.Denominator);
+    this.Numerator /= greatestCommonDivisor;
+    this.Denominator /= greatestCommonDivisor;
 };
 
-Fraction.prototype.subtract = function (other) {
-  let newDenominator = getDenominator(this, other);
 
-  let newNumerator =
-    this.Denominator === other.Denominator
-      ? this.Numerator - other.Numerator
-      : (newDenominator / other.Denominator) * this.Numerator -
-        (newDenominator / this.Denominator) * other.Numerator;
 
-  return new Fraction(newNumerator, newDenominator);
+Fraction.prototype.add = function(other) {
+    let newDenominator = getDenominator(this, other);
+
+    let newNumerator =
+        this.Denominator === other.Denominator
+            ? this.Numerator + other.Numerator
+            : (newDenominator / other.Denominator) * this.Numerator +
+            (newDenominator / this.Denominator) * other.Numerator;
+
+    this.Numerator = newNumerator;
+    this.Denominator = newDenominator;
+
+    this.reduce();
+    return this;
 };
 
-Fraction.prototype.multiply = function (other) {
-  return new Fraction(
-    this.Numerator * other.Numerator,
-    this.Denominator * other.Denominator
-  );
+Fraction.prototype.subtract = function(other) {
+    let newDenominator = getDenominator(this, other);
+
+    let newNumerator =
+        this.Denominator === other.Denominator
+            ? this.Numerator - other.Numerator
+            : (newDenominator / other.Denominator) * this.Numerator -
+            (newDenominator / this.Denominator) * other.Numerator;
+
+    this.Numerator = newNumerator;
+    this.Denominator = newDenominator;
+
+    this.reduce();
+    return this;
 };
 
-Fraction.prototype.divide = function (other) {
-  return this.multiply(new Fraction(other.Denominator, other.Numerator));
+Fraction.prototype.multiply = function(other) {
+    this.Numerator *= other.Numerator;
+    this.Denominator *= other.Denominator
+    this.reduce();
+
+    return this;
+};
+
+Fraction.prototype.divide = function(other) {
+    return this.multiply(new Fraction(other.Denominator, other.Numerator));
 };
 
 let f1 = new Fraction(1, 2);
 let f2 = new Fraction(1, 2);
 
-console.log(f1.add(f2));
+//console.log(f1.add(f2));
 console.log(f1.subtract(f2));
-console.log(f1.divide(f2));
+//console.log(f1.multiply(f2));
+//console.log(f1.divide(f2));
+
+
+//#endregion
+
+
+//#region Task3
+
+/*
+let time = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+    print: function() {
+        console.log(`${this.hours === 0 ? '00' : this.hours}:${this.minutes === 0 ? '00' : this.minutes}:${this.seconds === 0 ? '00' : this.seconds}`);
+    },
+    editHours: function(amount) {
+        this.hours += amount;
+        this.prettify();
+    },
+    editMinutes: function(amount) {
+        this.minutes += amount;
+        this.prettify();
+    },
+    editSeconds: function(amount) {
+        this.seconds += amount;
+        this.prettify();
+    },
+    prettify: function() {
+        this.hours += Math.floor(this.minutes / 60);
+        this.minutes = this.minutes % 60;
+
+        this.minutes += Math.floor(this.seconds / 60);
+        this.seconds = this.seconds % 60;
+    }
+}
+
+time.editMinutes(20);
+time.editSeconds(70);
+time.print();
+time.editSeconds(-20);
+time.print();
+*/
 
 //#endregion
