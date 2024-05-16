e03a5029151251ccf2c5bfea1a4b4e5f0d82b643
Фарманов Магомед Альбертович <Farma_gi04@itstep.edu.az>
2024-05-16 17:34:48 +0400

    details btn event handler

--- hw/hw_Farmanov_05.09.2024/js/Services/omdb-service.js
+++ hw/hw_Farmanov_05.09.2024/js/Services/omdb-service.js
@@ -1,18 +1,29 @@
-const url = 'http://www.omdbapi.com/?';
+const url = "http://www.omdbapi.com/?";
 
 class OmdbService {
-    constructor(key) {
-        this.apiKey = key;
-    }
+  constructor(key) {
+    this.apiKey = key;
+  }
 
-    async getDataAsync(title, type, page = 1) {
-        try {
-            const response = await fetch(`${url}apikey=${this.apiKey}&s=${title}&type=${type}&page=${page}`);
-            return response.json();
-        } catch (e) {
-            throw e;
-        }
+  async getDataAsync(title, type, page = 1) {
+    try {
+      const response = await fetch(
+        `${url}apikey=${this.apiKey}&s=${title}&type=${type}&page=${page}`
+      );
+      return response.json();
+    } catch (e) {
+      throw e;
     }
+  }
+
+  async getDataByIdAsync(id) {
+    try {
+      const response = await fetch(`${url}apikey=${this.apiKey}&i=${id}`);
+      return response.json();
+    } catch (e) {
+      throw e;
+    }
+  }
 }
 
 export default OmdbService;
--- hw/hw_Farmanov_05.09.2024/js/app.js
+++ hw/hw_Farmanov_05.09.2024/js/app.js
@@ -1,65 +1,70 @@
-import OmdbService from './Services/omdb-service.js'
+import OmdbService from "./Services/omdb-service.js";
 
 const omdbService = new OmdbService(82130204);
 
-const searchBtn = document.getElementById('search-btn');
-const moreBtn = document.getElementById('more-btn');
+const searchBtn = document.getElementById("search-btn");
+const moreBtn = document.getElementById("more-btn");
 
-const moviesList = document.getElementById('movies');
+const moviesList = document.getElementById("movies");
 
 let moreBtnHandler;
 
 searchBtn.onclick = async () => {
-    const title = document.getElementById('query-title').value;
-    const type = document.getElementById('query-type').value;
+  const title = document.getElementById("query-title").value;
+  const type = document.getElementById("query-type").value;
 
-    if (!title) {
-        return;
-    }
+  if (!title) {
+    return;
+  }
 
-    document.querySelector('.movies-container').style.display = 'flex';
+  document.querySelector(".movies-container").style.display = "flex";
 
-    const response = await omdbService.getDataAsync(title, type);
-    const movies = response.Search;
+  const response = await omdbService.getDataAsync(title, type);
+  const movies = response.Search;
 
-    moviesList.innerHTML = '';
+  moviesList.innerHTML = "";
 
-    movies.forEach(movie => moviesList.insertAdjacentHTML(
-        'beforeend',
-        getTemplate(movie))
-    );
+  movies.forEach((movie) => {
+    moviesList.insertAdjacentHTML("beforeend", getTemplate(movie));
 
-    moreBtn.style.display = 'block';
-    moreBtnHandler = loadMore(title, type);
+    document
+      .getElementById(`${movie.imdbID}`)
+      .addEventListener("click", async () => {
+        const response = await omdbService.getDataByIdAsync(movie.imdbID);
+        console.log(response);
+      });
+  });
+
+  moreBtn.style.display = "block";
+  moreBtnHandler = loadMore(title, type);
 };
 
 moreBtn.onclick = () => {
-    moreBtnHandler();
+  moreBtnHandler();
 };
 
 function loadMore(title, type) {
-    let counter = 2;
+  let counter = 2;
 
-    return async () => {
-        const response = await omdbService.getDataAsync(title, type, counter);
-        const movies = response.Search;
+  return async () => {
+    const response = await omdbService.getDataAsync(title, type, counter);
+    const movies = response.Search;
 
-        movies.forEach(movie => moviesList.insertAdjacentHTML(
-            'beforeend',
-            getTemplate(movie))
-        ); 
+    movies.forEach((movie) =>
+      moviesList.insertAdjacentHTML("beforeend", getTemplate(movie))
+    );
 
-        counter += 1;
-    }
+    counter += 1;
+  };
 }
 
 function getTemplate(movie) {
-    return `
+  return `
         <div class="movie-card">
             <img class="movie-poster" src="${movie.Poster}" alt="">
             <h4 style="padding: 0;">${movie.Title}</h4>
             <p style="padding: 0;margin: 0;">${movie.Year}</p>
-            <a class="btn" href="#">Details</a>
+            <a id="${movie.imdbID}" class="btn details-btn" href="#">Details</a>
         </div>
     `;
 }
